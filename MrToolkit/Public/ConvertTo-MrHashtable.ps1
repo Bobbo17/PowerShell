79b1a6380e62a1a4e960c375d767fe5cb4d3adcd
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 15:42:39 -0500

    Added comment based help to ConvertTo-MrHashTable

--- MrToolkit/Public/ConvertTo-MrHashtable.ps1
+++ MrToolkit/Public/ConvertTo-MrHashtable.ps1
@@ -1,4 +1,36 @@
 ﻿function ConvertTo-MrHashTable {
+
+<#
+.SYNOPSIS
+    Converts an array of PSObjects to an array of hashtables.
+
+.DESCRIPTION
+    The ConvertTo-MrHashTable function takes an array of PSObjects and converts each object into a
+    hashtable. Each property of the PSObject becomes a key in the hashtable with the corresponding
+    property value. This function can accept input directly from the pipeline.
+
+.PARAMETER Object
+    An array of PSObjects to be converted to hashtables. This parameter is mandatory and accepts
+    input from the pipeline.
+
+.EXAMPLE
+    ConvertTo-MrHashTable -Object (Get-Process)
+
+.EXAMPLE
+    Get-Process -Name pwsh | ConvertTo-MrHashTable
+
+.INPUTS
+    PSObject
+
+.OUTPUTS
+    Hashtable
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,
