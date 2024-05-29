9ee0249877ee077f56bd1ae9ce71bedcc3e8fb3b
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 15:48:14 -0500

    Added comment based help to Find-MrParameterAlias

--- MrToolkit/Public/Find-MrParameterAlias.ps1
+++ MrToolkit/Public/Find-MrParameterAlias.ps1
@@ -1,5 +1,39 @@
 #Requires -Version 3.0
 function Find-MrParameterAlias {
+
+<#
+.SYNOPSIS
+    Finds parameter aliases for the specified cmdlet.
+
+.DESCRIPTION
+    The Find-MrParameterAlias function retrieves the parameter aliases for a given cmdlet.
+    It can filter the parameters based on a specified parameter name pattern.
+
+.PARAMETER CmdletName
+    The name of the cmdlet for which to find parameter aliases. This parameter is mandatory.
+
+.PARAMETER ParameterName
+    The name of the parameter to filter the results by. Wildcards are allowed. Defaults to '*' to
+    retrieve all parameters.
+
+.EXAMPLE
+    Find-MrParameterAlias -CmdletName Get-Process
+
+.EXAMPLE
+    Find-MrParameterAlias -CmdletName Get-Process -ParameterName Name
+
+.INPUTS
+    String
+
+.OUTPUTS
+    PSCustomObject
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
