6f5d1f099c45b0a2d38e63999c37873322004c47
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:40:46 -0500

    Added comment based help to Test-MrFileLock

--- MrToolkit/Private/Test-MrFileLock.ps1
+++ MrToolkit/Private/Test-MrFileLock.ps1
@@ -1,4 +1,25 @@
 ﻿function Test-MrFileLock {
+
+<#
+.SYNOPSIS
+    Tests if a file is locked by another process.
+
+.DESCRIPTION
+    The Test-MrFileLock function checks if a specified file is currently locked by another process.
+    If the file can be opened for reading and writing, it is not locked; otherwise, it is locked.
+
+.PARAMETER Path
+    The path to the file to be checked. This parameter is required and must be a valid file path.
+
+.EXAMPLE
+    Test-MrFileLock -Path C:\path\to\file.txt
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [ValidateScript({
