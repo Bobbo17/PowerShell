4ade1da056d7cb464ada2c6792bc7c6568b9fc95
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:22:04 -0500

    Added comment based help to Get-MrScheduledTask

--- MrToolkit/Public/Get-MrScheduledTask.ps1
+++ MrToolkit/Public/Get-MrScheduledTask.ps1
@@ -1,5 +1,42 @@
 ﻿#Requires -Version 3.0
 function Get-MrScheduledTask {
+
+<#
+.SYNOPSIS
+    Retrieves scheduled tasks from specified computers.
+
+.DESCRIPTION
+    The Get-MrScheduledTask function retrieves scheduled tasks from one or more specified computers.
+    It can filter the tasks by name and can be run with alternate credentials if needed. The
+    function uses the schtasks.exe utility to query the scheduled tasks and returns the results as
+    custom objects.
+
+.PARAMETER ComputerName
+    The names of the computers from which to retrieve scheduled tasks. This parameter is mandatory.
+
+.PARAMETER TaskName
+    The name of the scheduled task to retrieve. If not specified, all scheduled tasks are retrieved.
+
+.PARAMETER Credential
+    The credentials to use for the remote connection. If not specified, the current user's
+    credentials are used.
+
+.EXAMPLE
+    Get-MrScheduledTask -ComputerName localhost
+
+.EXAMPLE
+    Get-MrScheduledTask -ComputerName RemotePC -TaskName '\MyTask'
+
+.EXAMPLE
+    $Cred = Get-Credential
+    Get-MrScheduledTask -ComputerName RemotePC1, RemotePC2 -Credential $Cred
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [ValidateNotNullOrEmpty()]
