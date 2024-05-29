520bf4000e3b2228eae934e692ee4dd87a6e0169
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:38:38 -0500

    Added comment based help to Test-MrURL

--- MrToolkit/Public/Test-MrURL.ps1
+++ MrToolkit/Public/Test-MrURL.ps1
@@ -1,5 +1,34 @@
 ﻿#Requires -Version 3.0 
 function Test-MrURL {
+
+<#
+.SYNOPSIS
+    Tests the accessibility of a specified URL.
+
+.DESCRIPTION
+    The Test-MrURL function sends a web request to a specified URL and checks if the request is
+    successful. The function can return a simple Boolean value indicating success or failure, or
+    detailed information about the request.
+
+.PARAMETER Uri
+    The URL to test. This parameter is required.
+
+.PARAMETER Detailed
+    If specified, the function returns detailed information about the web request, including status
+    code, status description, response URI, and server.
+
+.EXAMPLE
+    Test-MrURL -Uri http://www.example.com
+
+.EXAMPLE
+    Test-MrURL -Uri http://www.example.com -Detailed
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [string]$Uri,
