5d5fddf5485b44bab51d7fb266187f9bc07fea5c
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:36:08 -0500

    Added comment based help to Remove-MrSavedCredential

--- MrToolkit/Public/Remove-MrSavedCredential.ps1
+++ MrToolkit/Public/Remove-MrSavedCredential.ps1
@@ -1,6 +1,31 @@
 ﻿#Requires -Version 3.0
 function Remove-MrSavedCredential {
 
+<#
+.SYNOPSIS
+    Removes saved credentials for a specified target from the Windows Credential Manager.
+
+.DESCRIPTION
+    The Remove-MrSavedCredential function deletes saved credentials for a specified target from the
+    Windows Credential Manager. It supports the ShouldProcess pattern, allowing for confirmation
+    prompts and WhatIf scenarios.
+
+.PARAMETER Target
+    The target for which to remove saved credentials. This parameter is mandatory and accepts input
+    from the pipeline by property name.
+
+.EXAMPLE
+    Remove-MrSavedCredential -Target 'example.com'
+
+.EXAMPLE
+    'example.com', 'anotherexample.com' | Remove-MrSavedCredential
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding(SupportsShouldProcess,
                    ConfirmImpact='Medium')]
     param (
