18bbfc59718c92056923767f1150a02ef85e412a
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:18:15 -0500

    Added comment based help to Get-MrSavedCredential

--- MrToolkit/Public/Get-MrSavedCredential.ps1
+++ MrToolkit/Public/Get-MrSavedCredential.ps1
@@ -1,6 +1,28 @@
 ﻿#Requires -Version 3.0
 function Get-MrSavedCredential {
 
+<#
+.SYNOPSIS
+    Retrieves saved credentials for a specified target.
+
+.DESCRIPTION
+    The Get-MrSavedCredential function uses the cmdkey.exe utility to retrieve saved credentials
+    from the Windows Credential Manager for a specified target. The target string must not begin
+    with a forward slash.
+
+.PARAMETER Target
+    The target for which to retrieve saved credentials. This parameter must not begin with a
+    forward slash.
+
+.EXAMPLE
+    Get-MrSavedCredential -Target 'example.com'
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [ValidateScript({
