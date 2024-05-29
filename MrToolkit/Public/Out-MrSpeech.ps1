2cf2d4b17a0925a9d9b19355925fe60218f7ce8d
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:33:41 -0500

    Added comment based help to Out-MrSpeech

--- MrToolkit/Public/Out-MrSpeech.ps1
+++ MrToolkit/Public/Out-MrSpeech.ps1
@@ -1,4 +1,30 @@
 ﻿function Out-MrSpeech {
+
+<#
+.SYNOPSIS
+    Converts text to speech.
+
+.DESCRIPTION
+    The Out-MrSpeech function uses the System.Speech.Synthesis.SpeechSynthesizer class to convert
+    text input to speech. The function can accept input from the pipeline and will speak each
+    phrase it receives.
+
+.PARAMETER Phrase
+    The text phrase to be converted to speech. This parameter is mandatory and accepts input from
+    the pipeline.
+
+.EXAMPLE
+    Out-MrSpeech -Phrase 'Hello, World!'
+
+.EXAMPLE
+    'Hello', 'How are you?', 'Goodbye' | Out-MrSpeech
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,
