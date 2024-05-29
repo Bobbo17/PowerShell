8342655f2f33accde586cfcd7a163ea0a890d1f8
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 16:10:01 -0500

    Added comment based help to Get-MrPathSeparator

--- MrToolkit/Public/Get-MrPathSeparator.ps1
+++ MrToolkit/Public/Get-MrPathSeparator.ps1
@@ -1,3 +1,22 @@
 function Get-MrPathSeparator {
+
+<#
+.SYNOPSIS
+    Retrieves the platform-specific path separator character.
+
+.DESCRIPTION
+    The Get-MrPathSeparator function returns the character used to separate path strings in
+    environment variables specific to the platform on which PowerShell is running. This is useful
+    for scripts that need to handle paths in a cross-platform manner.
+
+.EXAMPLE
+    Get-MrPathSeparator
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
   [System.IO.Path]::PathSeparator
 }
