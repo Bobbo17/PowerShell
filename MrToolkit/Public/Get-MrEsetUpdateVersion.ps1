a3785fba21128ee0e3168d873c88d25e5f716b30
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-05-29 15:54:07 -0500

    Added comment based help to Get-MrEsetUpdateVersion

--- MrToolkit/Public/Get-MrEsetUpdateVersion.ps1
+++ MrToolkit/Public/Get-MrEsetUpdateVersion.ps1
@@ -1,4 +1,38 @@
 ﻿function Get-MrEsetUpdateVersion {
+
+<#
+.SYNOPSIS
+    Retrieves the ESET product update version information from specified computers.
+
+.DESCRIPTION
+    The Get-MrEsetUpdateVersion function connects to one or more specified computers and retrieves
+    ESET product update version information from the registry. The function can be run with
+    alternate credentials if needed.
+
+.PARAMETER ComputerName
+    The names of the computers to retrieve the ESET update version information from. Defaults to
+    the local computer if not specified.
+
+.PARAMETER Credential
+    The credentials to use for the remote connection. If not specified, the current user's
+    credentials are used.
+
+.EXAMPLE
+    Get-MrEsetUpdateVersion
+
+.EXAMPLE
+    Get-MrEsetUpdateVersion -ComputerName RemotePC
+
+.EXAMPLE
+    $Cred = Get-Credential
+    Get-MrEsetUpdateVersion -ComputerName RemotePC1, RemotePC2 -Credential $Cred
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [ValidateNotNullOrEmpty()]
