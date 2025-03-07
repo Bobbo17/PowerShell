e8a7e89b215097db1787385670dbc015b59f37be
Robert Durham <63553887+Bobbo17@users.noreply.github.com>
2025-03-07 15:22:53 -0700

    Update ICSToggle.bat

--- MrToolkit/ICSReset/ICSToggle.bat
+++ MrToolkit/ICSReset/ICSToggle.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Disabling ICS...
-powershell -ExecutionPolicy ByPass -command "Import-Module C:\ICSReset\Get-MrICS.ps1; Import-Module C:\ICSReset\Set-MrICS.ps1; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $false; echo Enabling ICS...; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $true;"
+powershell -ExecutionPolicy ByPass -command "Import-Module C:\ICSReset\Get-MrICS.ps1; Import-Module C:\ICSReset\Set-MrICS.ps1; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $false; echo 'Enabling ICS...'; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $true;"
 echo ICS Enabled
