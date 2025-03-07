d0a907a21d0b8a74e54d5f58b16612fb2ecc429e
Robert Durham <63553887+Bobbo17@users.noreply.github.com>
2025-03-07 15:05:21 -0700

    Add files via upload

---
+++ MrToolkit/ICSReset/ICSToggle.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Disabling ICS...
+powershell -ExecutionPolicy -ByPass -command "Import-Module C:\ICSReset\Get-MrInternetConnectionSharing.ps1; Import-Module C:\ICSReset\Set-MrInternetConnectionSharing.ps1; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $false; echo Enabling ICS...; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $true;"
+echo ICS Enabled
