edfb57749d4ca2ad89ce6e89919ce28dad1a6705
Robert Durham <63553887+Bobbo17@users.noreply.github.com>
2025-03-17 09:57:59 -0700

    Update ICSToggle.bat

--- MrToolkit/ICSReset/ICSToggle.bat
+++ MrToolkit/ICSReset/ICSToggle.bat
@@ -1,4 +1,5 @@
 @echo off
-echo Disabling ICS...
+echo Setting up Internet Connection Sharing for Credit Card Reader.
+echo Disabling ICS: Do not close this window, it will close automatically...
 powershell -ExecutionPolicy ByPass -command "Import-Module \Get-MrInternetConnectionSharing.ps1; Import-Module \Set-MrInternetConnectionSharing.ps1; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $false; echo 'Enabling ICS...'; Set-MrInternetConnectionSharing -InternetInterfaceName Wi-Fi -LocalInterfaceName Ethernet -Enabled $true;"
 echo ICS Enabled
